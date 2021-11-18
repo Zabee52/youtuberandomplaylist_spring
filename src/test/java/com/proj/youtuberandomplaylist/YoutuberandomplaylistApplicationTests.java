35a516005dc8026af000edad70127405fbf8265b
Zabee52 <zabeee52@gmail.com>
2021-11-18 09:35:38 +0900

    commit test
    잘 될까요?

---
+++ src/test/java/com/proj/youtuberandomplaylist/YoutuberandomplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.youtuberandomplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutuberandomplaylistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
