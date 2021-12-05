bd2abd2a36f6827157199d2384b60f217181b231
Zabee52 <zabeee52@gmail.com>
2021-12-05 21:45:44 +0900

    Update FeedController.java

--- src/main/java/com/proj/youtuberandomplaylist/controller/FeedController.java
+++ src/main/java/com/proj/youtuberandomplaylist/controller/FeedController.java
@@ -1,14 +1,13 @@
 package com.proj.youtuberandomplaylist.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FeedController {
     @GetMapping("/feed")
-    public String feed(Model model){
-        // 1. 로그인 상태가 아닐 경우 index 화면으로 redirect
+    public String feed(){
+        // 1. 로그인 상태가 아닐 경우 index 화면으로 redirect <- WebSecurity로 적용
         // 2. 내가 추가한 태그 목록 받아오기
         // 3. 내가 만든 플레이리스트 받아오기
         // 4. 내가 좋아요한 플레이리스트 받아오기
